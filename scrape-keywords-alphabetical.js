060caf700e9f8ec8cb64543d9949b3d36dc65cb5
Rick Christy <grymmjack@gmail.com>
2025-01-26 12:19:42 -0500

    Update scrape-keywords-alphabetical.js

--- scrape-keywords-alphabetical.js
+++ scrape-keywords-alphabetical.js
@@ -1,11 +1,11 @@
 console.clear();
 keywords = [];
-$('ul li a').each(function(i, el) { 
-  	if (kw = el.href.split('/')[5]) {
+$('ul li a').each(function(i, el) {
+        if (kw = el.href.split('/')[5]) {
       if (kw.indexOf(':') == -1) {
         switch(kw) {
-            case '%26B': 
-            	kw = '&B';
+            case '%26B':
+                kw = '&B';
             case '%26H':
               kw = '&H';
             case '%26O':
@@ -25,8 +25,18 @@ $('ul li a').each(function(i, el) {
       }
     }
     keywords.sort();
-  	keyset = new Set(keywords); 
-  	keywords = Array.from(keyset); 
-  	sout = keywords.join('\n'); 
-}); 
+        keyset = new Set(keywords);
+        keywords = Array.from(keyset);
+        sout = keywords.join('\n');
+    sout_lang_keywords = keywords.filter(function(el) {
+      return el.substring(0,1) != '$' && el.indexOf('(') == -1 && el.indexOf(')') == - 1 && el.indexOf('...') == -1;
+    }).join('|');
+    sout_lang_keywords_preprocessor = keywords.filter(function(el) {
+      return el.substring(0,1) == '$' && el.indexOf('(') == -1 && el.indexOf(')') == - 1 && el.indexOf('...') == -1;
+    }).join('|');
+    sout_lang_keywords = sout_lang_keywords.replaceAll('$', '\\\\$');
+        sout_lang_keywords_preprocessor = sout_lang_keywords_preprocessor.replaceAll('$', '\\\\$');
+});
 console.log(sout);
+console.log(sout_lang_keywords);
+console.log(sout_lang_keywords_preprocessor);
